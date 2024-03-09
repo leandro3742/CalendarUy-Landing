469756635f9e7c473471b1a5baddedff46051fcd
Leandro Marrero <leandro.marrero03@gmail.com>
2024-03-09 20:06:53 -0300

    primera publicacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {
+    colors: {
+      primary: '#2899CC'
     },
+    listStyleType: {
+      disc: 'disc',
+      decimal: 'decimal'
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: false, // or 'media' or 'class'
 }
